9605c20abbabe1fbe3c01c4209bef9b9a1b2fd41
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-04-19 13:50:44 +0200

    adding exercise

--- Warmup-2/string_times.py
+++ Warmup-2/string_times.py
@@ -1,1 +1,5 @@
-#
+# Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+def string_times(str, n):
+  new_string = str * n
+  return new_string
