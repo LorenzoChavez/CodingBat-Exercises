de293a1539484fbb2abe9530a18db814b88250f6
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-05-04 15:31:31 +0200

    adding exercise

---
+++ Logic-1/date_fashion.py
@@ -0,0 +1,14 @@
+# You and your date are trying to get a table at a restaurant. 
+# The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. 
+# The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). 
+# With the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).
+
+def date_fashion(you, date):
+  stylish = you >= 8 or date >= 8
+  no_style = you <= 2 or date <=2
+  if stylish and not no_style:
+    return 2
+  elif no_style:
+    return 0
+  else:
+    return 1
